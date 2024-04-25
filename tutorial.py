4d4f3b9f67fe7d8c8054d5fc38dc6b6db7fa8e75
Bradlin1 <bradlin9999@gmail.com>
2024-04-25 13:21:02 +0800

    u

--- tutorial.py
+++ tutorial.py
@@ -88,6 +88,15 @@ class Player (pygame.sprite.Sprite):
         self.ftime += 1
         self.update_sprite()
         print(fps)
+
+    def landed(self):
+        self.ftime = 0
+        self.y_vel = 0
+        self.jumptcount = 0
+
+    def hit_head(self):
+        self.count = 0
+        self.y_vel *= -1
         
     def update_sprite(self):
         sprite_sheet = "idle" #<-- defualt spreadsheet
@@ -152,13 +161,31 @@ def draw(window, background, bg_image, player, blocks):
 
     pygame.display.update()
 
-def handle_move(player):
+def handle_vertical_collision(player,objects,dy):
+    collided_obs = []
+    for obj in objects:
+        if pygame.sprite.collide_mask(player,obj):
+            if dy > 0:
+                player.rect.bottom = obj.rect.top #if moving down means colliding the top of this obj
+                player.landed()
+            elif dy < 0:
+                player.rect.top = obj.rect.bottom
+                player.hit_head()
+        collided_obs.append(obj)
+    return collided_obs 
+ 
+            
+
+
+def handle_move(player,objects):
     keyz = pygame.key.get_pressed( )
     player.x_vel = 0
     if keyz[pygame.K_a]:
         player.move_left(PLAYER_VEL)
     if keyz[pygame.K_d]:
         player.move_right (PLAYER_VEL)
+
+    handle_vertical_collision(player,objects,player.y_vel)
 
 def main(window):
     clock = pygame.time.Clock()
@@ -181,7 +208,7 @@ def main(window):
                 break
              
         player.loop(FPS)
-        handle_move(player)
+        handle_move(player,floor )
         draw(window, background, bg_image, player, floor)
 
     pygame.quit()
